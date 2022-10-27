5b6d9e44e9cf0d801a923d9073b989519d053bb5
leemheeje <dlagmleo89@gmail.com>
2022-10-27 16:54:16 +0900

    test3

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-//asdf
+//dev
 import '@/assets/style/scss/common.scss'
 
 Vue.config.productionTip = false
